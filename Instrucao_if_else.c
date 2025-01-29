9faf8d0d53572d9e363bf5941feee4bd5d231c3e
LucasCarvalhoAlmeida <lucas.almeida_bol@hotmail.com>
2025-01-29 01:29:12 +0000

    condicao_if_else

---
+++ Instrucao_if_else.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+int idade = 17;
+
+if (idade<=16) {
+
+printf("você não pode votar\n");
+}
+
+else {
+
+printf("Você pode votar\n");
+}
+  
+
+
+return 0;
+
+
+}
